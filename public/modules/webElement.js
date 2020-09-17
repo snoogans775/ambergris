ae0cde22186580e1090c9532ddb07a8fd37b2ce5
Kevin Fredericks <snoogans775@gmail.com>
2020-09-17 12:02:19 -0700

    updated static methods to new naming

--- public/modules/webElement.js
+++ public/modules/webElement.js
@@ -1,7 +1,7 @@
 //Custom web element creator
 export default function WebElement(obj) {
 	//Create new element
-	let ele = document.createElement(obj.element);
+	const ele = document.createElement(obj.element);
 	//Assign attributes for all correctly named object keys
 	//This relies on the string name in the object to be correct, dangerous
 	const keys = Object.keys(obj);

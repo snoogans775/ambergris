3da00f829507118989cc18ba7cbf1684d3388b6b
Kevin Fredericks <snoogans775@gmail.com>
2020-09-28 01:10:24 -0700

    add three.js canvas

--- public/charts.js
+++ public/charts.js
@@ -1,4 +1,43 @@
 import WebElement from "./modules/WebElement.js";
+import * as THREE from './modules/three/build/three.module.js';
+
+var camera, scene, light, renderer;
+var geometry, material, earthMesh;
+ 
+init();
+draw();
+ 
+function init() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 1000);
+    camera.position.z = 1.5;
+    light = new THREE.AmbientLight(0x888888)
+    scene.add(light);
+    light = new THREE.DirectionalLight( 'white', 1)
+    light.position.set(5,5,5)
+    light.target.position.set( 0,0,0 );
+    scene.add(light);
+
+    // Create mesh for earth texture
+    earthMesh = THREE
+
+    geometry = new THREE.SphereGeometry(0.5, 32, 32);
+    material = new THREE.MeshPhongMaterial();
+    earthMesh = new THREE.Mesh(geometry, material);
+    scene.add(earthMesh);
+
+    material.map = THREE.ImageUtils.loadTexture('img/earthmap1k.jpg');
+
+ 
+}
+ 
+function draw() {
+    renderer = new THREE.WebGLRenderer({
+        antialias: true,
+    });
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+}
 
 let svgContainer = new WebElement({
     element: 'svg',
@@ -32,5 +71,3 @@ navbar.enter()
     .append('div')
     .merge(navbar)
     .text( d => {return d;});
-
-d3.select('#content').text("test");

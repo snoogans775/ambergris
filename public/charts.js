08119fe1f728836bbd5147bc3bf1dab0587f1698
Kevin Fredericks <snoogans775@gmail.com>
2020-09-30 01:24:53 -0700

    cleanup of dependencies for charts.js

--- public/charts.js
+++ public/charts.js
@@ -1,66 +1,11 @@
-import WebElement from "./modules/WebElement.js";
-import * as THREE from './modules/three/build/three.module.js';
+import WebElement from './modules/WebElement.js';
+import * as Get from './modules/Requests.js';
 
-var camera, scene, light, renderer, textureLoader;
-var geometry, material, earthMesh;
- 
-init();
-draw();
- 
-function init() {
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 1000);
-    camera.position.z = 1.5;
-    light = new THREE.AmbientLight(0x888888)
-    scene.add(light);
-    light = new THREE.DirectionalLight( 'white', 1)
-    light.position.set(5,5,5)
-    light.target.position.set( 0,0,0 );
-    scene.add(light);
-
-    // Create mesh for earth texture
-    earthMesh = THREE
-
-    // Load textures
-    textureLoader = new THREE.TextureLoader();
-
-    geometry = new THREE.SphereGeometry(0.5, 32, 32);
-    material = new THREE.MeshPhongMaterial({
-        map: textureLoader.load('img/earthmap1k.jpg'),
-    });
-    earthMesh = new THREE.Mesh(geometry, material);
-    scene.add(earthMesh);
-}
- 
-function draw() {
-    renderer = new THREE.WebGLRenderer({
-        antialias: true,
-    });
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
-
-    renderer.render( scene, camera );
+const worldCovid = async () => {
+    return await Get.worldCovid();
 }
 
-let svgContainer = new WebElement({
-    element: 'svg',
-    width: "500",
-    height: "500"
-});
-
-let rect = new WebElement({
-    element: "rect",
-    x: "500",
-    y: "0",
-    width: "300",
-    height: "200",
-    fill: "red"
-});
-
-let test = new WebElement({
-    element: "div",
-    class: "testy"
-});
+const data = worldCovid();
 
 //Add dummy data to to the navbar
 let pages = ['Explore', 'Global', 'United States'];
